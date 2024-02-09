31c53674714bcbf1bbabdf9d08ae104cf048ac27
Mello Birkan <mgbirkan@icloud.com>
2024-02-09 04:15:29 -0300

    First Commit

--- actix_overview/src/main.rs
+++ actix_overview/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::HttpServer;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        actix_web::App::new()
+            .route("/", actix_web::web::get().to(index))
+    })
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
+}
+
+async fn index() -> &'static str {
+    "hello, world"
 }
